c6872e87b1984f41c85fef664a0cc65470c7942f
gabriel <gabrielmmagalhaes19@gmail.com>
2023-03-22 11:50:38 -0300

    primeiro commit

---
+++ app.py
@@ -0,0 +1,23 @@
+import os
+import smtplib
+from email.message import EmailMessage
+
+# Configurar email, senha
+
+EMAIL_ADRESS = ''    # Email
+EMAIL_PASSWORD = ''  # Senha do email
+
+# Criando a mensagem
+
+msg = EmailMessage()
+msg['Subject'] = 'Carga chegou ao porto'
+msg['From'] = ''  # Quem está enviando o email
+msg['To'] = ''    # Quem está recebendo
+
+msg.set_content('Favor buscar a carga que acabou de chegar na portaria')
+
+# Enviando o email
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADRESS, EMAIL_PASSWORD)
+    smtp.send_message(msg)
